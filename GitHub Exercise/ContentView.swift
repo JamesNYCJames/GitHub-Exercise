908c660f7197a0bae7b8f766c444c53167d30f44
James <magdiejamesny@gmail.com>
2023-09-17 21:49:50 -0400

    Initial Commit

---
+++ GitHub Exercise/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  GitHub Exercise
+//
+//  Created by Charrel, James on 9/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+        Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+                .padding()
+        
+        Text("Hello, 1.1!")
+                .font(.largeTitle)
+            .padding()
+    }
+        .foregroundColor(.red)
+}
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Charrel, James on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
