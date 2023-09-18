6de7908f35d646531a9f1cf0741bbabb4a965e44
James <magdiejamesny@gmail.com>
2023-09-17 22:04:28 -0400

    development - added rectangle

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -19,6 +19,8 @@ struct ContentView: View {
         Text("Hello, 1.2!")
                 .font(.largeTitle)
             .padding()
+            Rectangle()
+                .padding()
     }
         .foregroundColor(.green)
 }
